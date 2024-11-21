75d6e64200c46813a7185d8ff94b7feaac28c1af
JVRALPH <73514284+JVRALPH@users.noreply.github.com>
2024-11-21 16:18:44 -0600

    Actualizar creacion_data.py

--- KanjiCNN/Modelo/creacion_data.py
+++ KanjiCNN/Modelo/creacion_data.py
@@ -8,13 +8,13 @@ from configparser import ConfigParser
 # Configuración de rutas a partir del archivo
 config = ConfigParser()
 config.read("/content/Project/Proyecto/New/config.ini")
-RUTA_CLASES = config["Rutas"]["CLASS_PATH"]
-RUTA_PREDICCION = config["Rutas"]["PREDICT_PATH"]
-RUTA_MODELO = config["Rutas"]["MODEL_PATH"]
-RUTA_IMAGENES_NO_PROCESADAS = config["Rutas"]["UNPROCESSED_IMAGES_PATH"]
-RUTA_IMAGENES_ENTRENAMIENTO = config["Rutas"]["TRAINING_IMAGES_PATH"]
+RUTA_CLASES = config["Rutas"]["RUTA_CLASES"]
+RUTA_PREDICCION = config["Rutas"]["RUTA_PREDICCION"]
+RUTA_MODELO = config["Rutas"]["RUTA_MODELO"]
+RUTA_IMAGENES_NO_PROCESADAS = config["Rutas"]["RUTA_IMAGENES_NO_PROCESADAS"]
+RUTA_IMAGENES_ENTRENAMIENTO = config["Rutas"]["RUTA_IMAGENES_ENTRENAMIENTO"]
 
-TAMANIO_LOTE = 128  # Definir el tamaño del lote
+TAMANO_LOTE = 128  # Definir el tamaño del lote
 
 # Crear archivo CSV para etiquetas
 claves_clases = sorted(os.listdir(RUTA_IMAGENES_ENTRENAMIENTO))
@@ -22,7 +22,7 @@ with open(RUTA_CLASES, "w") as archivo_clases:
     archivo_clases.write(",".join(claves_clases))
 
 # Imprimir el inicio del proceso de creación del conjunto de entrenamiento
-print("\n================= Creación del conjunto de entrenamiento =================\n")
+print("\nCreación del conjunto de entrenamienton\n")
 
 # Secuencia para la aumentación de datos
 aumentacion_datos = keras.Sequential([
@@ -37,7 +37,7 @@ conjunto_entrenamiento = keras.preprocessing.image_dataset_from_directory(
     labels="inferred",
     label_mode="int",
     color_mode="grayscale",
-    batch_size=TAMANIO_LOTE,
+    batch_size=TAMANO_LOTE,
     image_size=(64, 64),
     shuffle=True,
     seed=420,
@@ -59,7 +59,7 @@ conjunto_validacion = keras.preprocessing.image_dataset_from_directory(
     labels="inferred",
     label_mode="int",
     color_mode="grayscale",
-    batch_size=TAMANIO_LOTE,
+    batch_size=TAMANO_LOTE,
     image_size=(64, 64),
     shuffle=True,
     seed=420,
